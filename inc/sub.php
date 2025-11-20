7ae89f6370f83cda83f32e69255ea7b2efa3db03
EwanKM <starewan78@gmail.com>
2025-11-20 11:44:10 +0100

    Correction problème d'inscription infini Fix #15

--- inc/sub.php
+++ inc/sub.php
@@ -1,8 +1,31 @@
 <?php
-    if(!empty($_REQUEST['valid'])) {
-        Auth::getInstance()->subscribe($_REQUEST['login'], $_REQUEST['pwd']);
-        echo '<p>Inscription réalisée avec succès !</p>';
+
+session_start();
+
+$limite = 3;     
+$duree = 60; 
+
+if (!isset($_SESSION['attempts'])) {
+    $_SESSION['attempts'] = [];
+}
+
+$_SESSION['attempts'] = array_filter(
+    $_SESSION['attempts'],
+    function ($t) use ($duree) {
+        return $t > time() - $duree;
     }
+);
+
+if (count($_SESSION['attempts']) >= $limite) {
+    die("Trop d'inscriptions ! , veuillez réessayer dans une minute :D");
+}
+
+$_SESSION['attempts'][] = time();
+
+if(!empty($_REQUEST['valid'])) {
+    Auth::getInstance()->subscribe($_REQUEST['login'], $_REQUEST['pwd']);
+    echo '<p>Inscription réalisée avec succès !</p>';
+}
 ?><form method="post">
     <div>
         <h2>Inscription</h2>
